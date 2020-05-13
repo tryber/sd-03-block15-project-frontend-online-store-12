316c11592e1084f256848104bb46166dcb4fbcdd
Gabriel <dreadpool38@gmail.com>
2020-05-13 17:17:41 -0300

    Add card 9 functions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import SearchItems from './components/SearchItems';
 import './App.css';
 import CategoryBar from './components/CategoryBar';
+import QuantitySelector from './components/QuantitySelector';
+import EmptyCart from './components/EmptyCart';
+
 
 function App() {
   return (
     <div>
-      <CategoryBar />
-      <SearchItems />
+      <BrowserRouter>
+        <CategoryBar />
+        <SearchItems />
+        <QuantitySelector />
+        <EmptyCart />
+      </BrowserRouter>
     </div>
 
 
