9532748d13efb79bf68f406ae7a1fea64de50be8
Gabriel <dreadpool38@gmail.com>
2020-05-13 17:38:41 -0300

    CodeClimate update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-
 import { BrowserRouter, Route } from 'react-router-dom';
 import EmptyCart from './components/EmptyCart';
 import ShoppingCart from './pages/ShoppingCart';
 import SearchItems from './components/SearchItems';
 import './App.css';
 import CategoryBar from './components/CategoryBar';
-import QuantitySelector from './components/QuantitySelector';
-import EmptyCart from './components/EmptyCart';
 
 
 function App() {
@@ -21,7 +18,6 @@ function App() {
         <SearchItems />
       </div>
     </BrowserRouter>
-
 
 
   );
