bced3c19c7915de18ae02173af1eb218a4478f2b
ilan274 <ilan.herbach@gmail.com>
2020-05-13 18:50:26 -0300

    Card 5.1 5.2

---
+++ src/components/ProductList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import SearchMessage from './SearchMessage';
+
+class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.products.length && this.state.products.map((product) => (
+          <p data-testid="product" key={product.id}>{product.title}</p>
+        ))}
+        <SearchMessage items={(xablau) => this.setState(xablau)} />
+      </div>
+    );
+  }
+}
+
+export default ProductList;
