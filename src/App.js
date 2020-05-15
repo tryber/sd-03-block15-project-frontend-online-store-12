960a0f1186f3b5e876fbb56d34d1950a1adf10cb
Lourenço <loulage@gmail.com>
2020-05-14 21:59:49 -0300

    Resolvendo render do App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ShoppingCart from './pages/ShoppingCart';
 import Home from './pages/Home';
 import './App.css';
-import ProductDetails from './pages/ProductDetails';
+import { ProductDetails } from './pages/ProductDetails';
 
 
 function App() {
@@ -13,9 +13,9 @@ function App() {
       <div className="App">
 
         <Switch>
-          <Route path="/" component={Home} />
           <Route path="/pages/shopping-cart" component={ShoppingCart} />
-          <Route path="/productdetails/:id" render={() => <ProductDetails />} />
+          <Route path="/productdetails/:id" component={ProductDetails} />
+          <Route exact path="/" component={Home} />
         </Switch>
 
 
