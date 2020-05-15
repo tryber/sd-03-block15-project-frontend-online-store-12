209c8d894f95006e73d193eb9fec6e1b24cbff7f
Lourenço <loulage@gmail.com>
2020-05-15 17:33:45 -0300

    Adicionando freeshipping em details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -4,43 +4,29 @@ import AvaliationForm from '../components/AvaliationForm';
 import QuantitySelector from '../components/QuantitySelector';
 
 export class ProductDetails extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { quantidade: 0 };
-    this.setQuantity = this.setQuantity.bind(this);
-  }
-
-  setQuantity(quantidade) {
-    this.setState({ quantidade });
-  }
-
   render() {
-    const { quantidade } = this.state;
-    console.log(this.props);
-    const { location: { state: { product } }, setItemToCart } = this.props;
-    const { id, price, title, thumbnail, attributes } = product;
+    const { location: { state: { product } } } = this.props;
+    const { price, title, thumbnail, attributes, shipping } = product;
+    const { free_shipping: freeShipping } = shipping;
     return (
       <div className="product-detail">
         <div className="product-detail-header">
           <Link to="/">Voltar</Link>
-          <h3 data-testid="product-detail-name">{`${title} - R$ ${price}`}</h3>
+          <h3 data-testid="product-detail-name">{`${title} R$ ${price}`}</h3>
         </div>
-        <img className="product-detail-img" src={thumbnail} alt="" />
+        <div className="product-detail-img">
+          <img src={thumbnail} alt="" />
+        </div>
         <div className="product-detail-specs">
           <h3>Especificações técnicas</h3>
           {attributes.map((att) => (
             <li key={att.name}>{`${att.name} : ${att.value_name}`}</li>
           ))}
         </div>
-        <QuantitySelector setQuantity={this.setQuantity} />
-        <button
-          type="button"
-          onClick={() => setItemToCart({ id, title, thumbnail, price }, quantidade)}
-          data-testid="product-add-to-cart"
-        >
-          Adicionar ao Carrinho
-        </button>
+        <div data-testid="free-shipping">
+          { freeShipping && <FreeShipping /> }
+        </div>
+        <QuantitySelector />
         <AvaliationForm />
       </div>
     );
